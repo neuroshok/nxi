a8b3660613c087adb25ca08197049e335d1a2191
ads00 <adess00@hotmail.fr>
2019-09-13 22:13:59 +0200

    [update] config

--- include/nxi/config.hpp
+++ include/nxi/config.hpp
@@ -5,7 +5,7 @@
 
 #include <string>
 
-#define nxi_config_open struct : ndb::persistent_group { using persistent_group::persistent_group;
+#define nxi_config_open(NAME) struct : ndb::persistent_group { using persistent_group::persistent_group;
 #define nxi_config_close(GROUP_NAME) } GROUP_NAME{ this, #GROUP_NAME };
 #define nxi_config_key(NAME, TYPE, DEFAULT_VALUE, DESCRIPTION) nxi::config_key<TYPE> NAME{ this, #NAME, DEFAULT_VALUE, DESCRIPTION };
 
@@ -31,17 +31,20 @@ namespace nxi
     {
         using persistent_group::persistent_group;
 
-        nxi_config_open
+        nxi_config_open(browser)
             nxi_config_key(home, std::string, "www.google.com", "");
             nxi_config_key(download_path, std::string, "d:/incoming", "");
-            nxi_config_key(page_unload_timeout, int, 20, "Duration before unloading a closed page");
 
-            nxi_config_open
+            nxi_config_key(page_unload_timeout, int, 20, "Duration before unloading a closed page");
+            nxi_config_key(page_mute_mode, int, 0, "0: Normal\n1: Auto mute non-active pages");
+            nxi_config_key(page_fullscreen_mode, int, 0, "0: Fullscreen\n1: Fullwindow\n2: Hybrid");
+
+            nxi_config_open(history)
                 nxi_config_key(preview_size, int, 10, "");
                 nxi_config_key(suggest_ratio, double, 0.2, "");
             nxi_config_close(history)
 
-            nxi_config_open
+            nxi_config_open(interface)
                 nxi_config_key(show_page_bar, bool, true, "");
             nxi_config_close(interface)
 
