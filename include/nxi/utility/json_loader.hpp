2e09ac12d528d6f1048b0ce6805ed0a46c99aa52
ads00 <adess00@hotmail.fr>
2019-09-28 23:29:01 +0200

    [update] json_loader

--- include/nxi/utility/json_loader.hpp
+++ include/nxi/utility/json_loader.hpp
@@ -12,9 +12,10 @@
 
 #include <QString>
 #include <QSize>
+#include <QImage>
 
 #define nxi_json_key_overload2(NAME, TYPE) ::nxi::json_value<TYPE> NAME{ this, #NAME };
-#define nxi_json_key_overload3(NAME, TYPE, DEFAULT_VALUE) ::nxi::json_value<TYPE> NAME{ this, #NAME, DEFAULT_VALUE };
+#define nxi_json_key_overload3(NAME, TYPE, DEFAULT_VALUE) ::nxi::json_value<TYPE> NAME{ this, #NAME, TYPE DEFAULT_VALUE };
 
 #define nxi_json_open(GROUP_NAME) struct : ::nxi::json_object { using json_object::json_object;
 #define nxi_json_close(GROUP_NAME) } GROUP_NAME{ this, #GROUP_NAME };
@@ -50,20 +51,26 @@ namespace nxi
         {}
 
         const T& get() const { return value_; }
+        void set(const T& v) { value_ = v; }
+        bool is_null() const { return is_null_; }
 
     private:
         T init_value(json_object* parent, const QString& key, const T& default_value)
         {
+            is_null_ = false;
             if (!parent->value_.toObject()[key].isNull()) return json_load_value<T>(parent->value_.toObject()[key]);
+            is_null_ = true;
             return default_value;
         }
 
     private:
+        bool is_null_;
         T value_;
     };
 
-    template<>
-    QSize json_load_value<QSize>(const QJsonValue&);
+    template<> int json_load_value<int>(const QJsonValue&);
+    template<> QSize json_load_value<QSize>(const QJsonValue&);
+    template<> QImage json_load_value<QImage>(const QJsonValue&);
 } // nxi
 
 #endif // INCLUDE_NXI_CONFIG_JSON_LOADER_HPP_NXI
--- source/nxi/utility/json_loader.cpp
+++ source/nxi/utility/json_loader.cpp
@@ -10,10 +10,22 @@ namespace nxi
     {}
 
     template<>
+    int json_load_value<int>(const QJsonValue& value)
+    {
+        return value.toInt();
+    }
+
+    template<>
     QSize json_load_value<QSize>(const QJsonValue& value)
     {
         QJsonArray array{ value.toArray() };
         nxi_assert(array.size() == 2);
         return QSize{ array[0].toInt(), array[1].toInt() };
+    }
+
+    template<>
+    QImage json_load_value<QImage>(const QJsonValue& value)
+    {
+        return QImage{ value.toString() };
     }
 } // nxi
