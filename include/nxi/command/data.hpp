d1fc5efa382dddb731dfb36acf2b418cbd7b23e9
ads00 <adess00@hotmail.fr>
2019-10-17 01:39:01 +0200

    [add] command data

---
+++ include/nxi/command/data.hpp
@@ -0,0 +1,20 @@
+#ifndef INCLUDE_NXI_COMMAND_DATA_HPP_NXI
+#define INCLUDE_NXI_COMMAND_DATA_HPP_NXI
+
+#include <nxi/command/fwd.hpp>
+
+#include <QString>
+
+namespace nxi
+{
+	struct command_data
+    {
+	    QString module = "nxi";
+		QString action = "none";
+		nxi::command_function_type function;
+        QString icon;
+        QString description;
+    };
+} // nxi
+
+#endif // INCLUDE_NXI_COMMAND_DATA_HPP_NXI
