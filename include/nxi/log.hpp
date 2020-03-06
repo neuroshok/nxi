da2f9cb5e3c99aade46d5ab1a7c55ec502a5796b
ads00 <adess00@hotmail.fr>
2020-03-06 03:41:00 +0100

    [fix] gcc/clang compilation

--- include/nxi/log.hpp
+++ include/nxi/log.hpp
@@ -18,7 +18,7 @@ OStream& operator<<(OStream &os, const QString& c)
 }
 
 #if defined(__GNUC__) || defined(__clang__)
-    #define nxi_unreachable() void((__builtin_unreachable())
+    #define nxi_unreachable() void(__builtin_unreachable())
 #elif defined(_MSC_VER) && _MSC_VER >= 1900
     #define nxi_unreachable() void(__assume(false))
 #else
