5990a08ec026aa0cf9ac008d06e7cbab3d38cf77
ads00 <ads.neuroshok@gmail.com>
2020-04-01 21:07:25 +0200

    [refactor] remove test module

--- source/module/test/background.js
+++
@@ -1,5 +0,0 @@
-function onInstalledNotification(details) {
-  browser.windows.create();
-}
-
-browser.runtime.onInstalled.addListener(onInstalledNotification);
--- source/module/test/test.js
+++
@@ -1,2 +0,0 @@
-document.body.style.border = "5px solid blue";
-browser.windows.create();
