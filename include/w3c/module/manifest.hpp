51c1af10e39248be042ac22d592dfaac1e1d7635
ads00 <ads.neuroshok@gmail.com>
2020-04-06 20:23:08 +0200

    [update] web_module manifest

--- include/w3c/module/manifest.hpp
+++ include/w3c/module/manifest.hpp
@@ -3,6 +3,8 @@
 
 //! https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/manifest.json
 
+#include <unordered_map>
+#include <variant>
 #include <vector>
 
 #include <QString>
@@ -11,16 +13,75 @@ namespace w3c
 {
     struct manifest
     {
+        QString author;
         QString name;
+
+        struct background_t
+        {
+            QString page;
+            bool persistent;
+            std::vector<QString> scripts;
+        } background;
 
         struct browser_action_t
         {
+            struct theme_icons_t
+            {
+                QString dark;
+                QString light;
+                int size;
+            };
+
             bool browser_style = false;
             QString default_area;
-            QString default_icon;
+            std::variant<QString, std::unordered_map<int, QString>> default_icon;
             QString default_popup;
             QString default_title;
+            std::vector<theme_icons_t> theme_icons;
         } browser_action;
+
+        // ignore browser_specific_settings
+
+        struct chrome_settings_overrides_t
+        {
+            struct search_provider_t
+            {
+                QString name;
+                QString search_url;
+                bool is_default;
+                std::vector<QString> alternate_urls;
+                QString encoding;
+                QString favicon_url;
+                QString image_url;
+                QString image_url_post_params;
+                QString instant_url;
+                QString instant_url_post_params;
+                QString keyword;
+                int prepopulated_id;
+                QString search_url_post_params;
+                QString suggest_url;
+                QString suggest_url_post_params;
+            };
+            QString homepage;
+        } chrome_settings_overrides;
+
+        struct chrome_url_overrides_t
+        {
+            QString bookmarks;
+            QString history;
+            QString newtab;
+        } chrome_url_overrides;
+
+        struct command_t
+        {
+            QString suggested_key; // use single shortcut, ignore os specific shortcut from w3c
+            QString description;
+            // special actions
+            QString execute_browser_action;
+            QString execute_page_action;
+            QString execute_sidebar_action;
+        };
+        std::vector<std::pair<QString, command_t>> commands;
 
         struct content_scripts_t
         {
@@ -36,6 +97,95 @@ namespace w3c
             run_at_type run_at;
         };
         std::vector<content_scripts_t> content_scripts;
+
+        QString content_security_policy;
+        QString default_locale;
+        QString description;
+
+        struct developer_t
+        {
+            QString name;
+            QString url;
+        };
+
+        QString devtools_page;
+
+        // dictionaries // what is this ?
+
+        struct externally_connectable_t
+        {
+            std::vector<QString> ids;
+            std::vector<QString> matches;
+        } externally_connectable;
+
+        QString homepage_url;
+        std::unordered_map<int, QString> icons;
+        enum class incognito{ not_allowed, spanning, split };
+        int manifest_version;
+        QString name;
+
+        struct omnibox_t
+        {
+            QString keyword;
+        } omnibox;
+
+        std::vector<QString> optional_permissions;
+
+        struct options_ui_t
+        {
+            bool browser_style;
+            bool open_in_tab;
+            QString page;
+        } options_ui;
+
+        struct page_action_t
+        {
+            struct theme_icons_t
+            {
+                QString dark;
+                QString light;
+                int size;
+            };
+
+            bool browser_style = false;
+            std::variant<QString, std::unordered_map<int, QString>> default_icon;
+            QString default_popup;
+            QString default_title;
+            std::vector<QString> hide_matches;
+            std::vector<QString> show_matches;
+            bool pinned = true;
+        } page_action;
+
+        std::vector<QString> permissions;
+
+        struct protocol_handlers_t
+        {
+            QString protocol;
+            QString name;
+            QString uriTemplate;
+        } protocol_handlers;
+
+        QString short_name;
+
+        // sidebar_action // unsupported in nxi ui
+
+        struct storage_t
+        {
+            QString managed_schema;
+        } storage;
+
+        //! themes are not part of modules in nxi
+        // theme
+        // theme_experiment
+
+        struct user_scripts_t
+        {
+            QString api_script;
+        } user_scripts;
+
+        QString version;
+        QString version_name;
+        std::vector<QString> web_accessible_resources;
 
         manifest(const QString& name);
         void load(const QString& name);
--- source/w3c/manifest.cpp
+++ source/w3c/manifest.cpp
@@ -1,4 +1,4 @@
-#include <w3c/module//manifest.hpp>
+#include <w3c/module/manifest.hpp>
 #include <nxi/log.hpp>
 
 #include <vector>
@@ -29,47 +29,42 @@ namespace w3c
             auto root = doc.object();
             name = root["name"].toString();
 
-            auto browser_action_obj = root["browser_action"].toObject();
-            browser_action.browser_style = browser_action_obj["browser_style"].toBool();
-            browser_action.default_area = browser_action_obj["default_area"].toString();
-            browser_action.default_icon = browser_action_obj["default_icon"].toString();
-            browser_action.default_popup = browser_action_obj["default_popup"].toString();
-            browser_action.default_title = browser_action_obj["default_title"].toString();
+            if (!root["background"].isNull())
+            {
+                auto background_obj = root["background"].toObject();
+                background.page = background_obj["page"].toString();
+                background.persistent = background_obj["persistent"].toBool();
+                for (const auto& item : background_obj["scripts"].toArray()) background.scripts.push_back(item.toString());
+            }
 
-            for (auto& item : root["content_scripts"].toArray())
+            if (!root["browser_action"].isNull())
+            {
+                auto browser_action_obj = root["browser_action"].toObject();
+                browser_action.browser_style = browser_action_obj["browser_style"].toBool();
+                browser_action.default_area = browser_action_obj["default_area"].toString();
+                browser_action.default_icon = browser_action_obj["default_icon"].toString();
+                browser_action.default_popup = browser_action_obj["default_popup"].toString();
+                browser_action.default_title = browser_action_obj["default_title"].toString();
+                for (const auto& item : browser_action_obj["theme_icons"].toArray())
+                {
+                    auto theme_icons_obj = item.toObject();
+                    browser_action.theme_icons.push_back(
+                        browser_action_t::theme_icons_t{
+                            theme_icons_obj["light"].toString()
+                            , theme_icons_obj["light"].toString()
+                            , theme_icons_obj["size"].toInt()}
+                        );
+                }
+            }
+
+            for (const auto& item : root["content_scripts"].toArray())
             {
                 content_scripts_t cs;
                 auto content_script_item = item.toObject();
-                for (auto& value : content_script_item["matches"].toArray()) cs.matches.push_back(value.toString());
-                for (auto& value : content_script_item["js"].toArray()) cs.js.push_back(value.toString());
+                for (const auto& value : content_script_item["matches"].toArray()) cs.matches.push_back(value.toString());
+                for (const auto& value : content_script_item["js"].toArray()) cs.js.push_back(value.toString());
                 content_scripts.push_back(std::move(cs));
             }
         }
     }
 } // w3c
-
-namespace nxi
-{
-    /*
-    template<>
-    std::vector<nxi::web_module::content_script> json_load_value<std::vector<nxi::web_module::content_script>>(const QJsonValue& values)
-    {
-        std::vector<nxi::web_module::content_script> vector;
-        for (auto& obj : values.toArray())
-        {
-            std::vector<QString> matches;
-            std::vector<QString> js;
-            for (auto& value : obj.toObject()["matches"].toArray()) matches.push_back(value.toString());
-            for (auto& value : obj.toObject()["js"].toArray()) js.push_back(value.toString());
-
-            web_module::content_script cs;
-            cs.matches = std::move(matches);
-            cs.js = std::move(js);
-
-            vector.emplace_back(std::move(cs));
-        }
-        return vector;
-    }
-
-     */
-} // nxi
