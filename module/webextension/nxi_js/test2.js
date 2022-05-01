5050fbe1a803f5841a9f89725c1f09a237fafc84
ads00 <ads.neuroshok@gmail.com>
2022-05-01 09:11:04 +0200

    [remove] module directory

--- module/webextension/nxi_js/test2.js
+++
@@ -1,8 +0,0 @@
-document.body.style.border = "5px solid blue";
-document.body.innerHTML = "<div id = 'zeta'>nxi_js module</div>" + document.body.innerHTML;
-
-function test(activeInfo) {
-    document.getElementById("zeta").innerHTML = "current page id : " + activeInfo.tabId;
-}
-
-browser.tabs.onActivated.addListener( test );
