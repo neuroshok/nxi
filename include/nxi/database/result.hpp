1ebae524fb1374af59245602daad56f031e628bc
ads00 <ads.neuroshok@gmail.com>
2022-05-02 20:15:43 +0200

    [update] add warning to nxi::result

--- include/nxi/database/result.hpp
+++ include/nxi/database/result.hpp
@@ -2,7 +2,9 @@
 #define INCLUDE_NXI_DATABASE_RESULT_HPP_NXI
 
 #include <nxi/database/field.hpp>
+#include <nxi/log.hpp>
 #include <QSqlQuery>
+#include <QSqlError>
 
 namespace nxi
 {
@@ -11,7 +13,7 @@ namespace nxi
     public:
         result(QSqlQuery& query) : query_{ query }
         {
-            query_.exec();
+            if (!query_.exec()) nxi_error("query error : {} : {}", query_.lastError().text(), query_.lastQuery());
         }
 
         bool next() const { return query_.next(); }
@@ -35,6 +37,7 @@ namespace nxi
             else if constexpr (std::is_same_v<typename Field::type, bool>) { return query_.value(Field::index).toBool(); }
             else if constexpr (std::is_same_v<typename Field::type, QByteArray>) { return query_.value(Field::index).toByteArray(); }
             else if constexpr (std::is_same_v<typename Field::type, QString>) { return query_.value(Field::index).toString(); }
+            else nxi_warning("Unknown type");
         }
 
     private:
