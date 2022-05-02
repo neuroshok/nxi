2d53496569d08ce506cf7dfc485bf7e1867b4d16
ads00 <ads.neuroshok@gmail.com>
2022-05-03 00:17:27 +0200

    [update] user agent

--- source/nxi/web_session.cpp
+++ source/nxi/web_session.cpp
@@ -41,7 +41,7 @@ namespace nxi
         settings()->setAttribute(QWebEngineSettings::ScrollAnimatorEnabled, true);
         settings()->setAttribute(QWebEngineSettings::FullScreenSupportEnabled, true);
         setNotificationPresenter([this](std::unique_ptr<QWebEngineNotification> notif) { session_.error(notif->message()); });
-        setHttpUserAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:10.0) Gecko/20100101 Firefox/99.0.1");
+        setHttpUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0");
     }
 
     void web_session::load_cookie(const QString& domain)
