54274e3b01661e937898c9ec8e411622367aca7a
ads00 <ads.neuroshok@gmail.com>
2022-05-07 17:51:58 +0200

    [fix] clang format

--- include/nxi/data/context_struct.hpp
+++ include/nxi/data/context_struct.hpp
@@ -1,8 +1,14 @@
+// clang-format off
+
 #include <nxi/data/init.hpp>
 
 #include <QString>
 
-init_open(context) init_field(0, QString, name, "text") init_field(1, unsigned int, priority, "integer default 1")
-init_field(2, int, active, "integer default 0") init_SQL("PRIMARY KEY(`name`)") init_close(context)
+init_open(context)
+    init_field(0, QString, name, "text")
+    init_field(1, unsigned int, priority, "integer default 1")
+    init_field(2, int, active, "integer default 0")
+    init_SQL("PRIMARY KEY(`name`)")
+init_close(context)
 
 #include <nxi/data/deinit.hpp>
--- include/nxi/data/module_struct.hpp
+++ include/nxi/data/module_struct.hpp
@@ -1,10 +1,17 @@
+// clang-format off
+
 #include <nxi/data/init.hpp>
 
 #include <nxi/type.hpp>
 
 #include <QString>
 
-init_open(module) init_field(0, int, id, "integer") init_field(1, QString, name, "text") init_field(2, nxi::module_type, type, "integer")
-init_field(3, bool, active, "integer") init_SQL("PRIMARY KEY(`id`)") init_close(module)
+init_open(module)
+    init_field(0, int, id, "integer")
+    init_field(1, QString, name, "text")
+    init_field(2, nxi::module_type, type, "integer")
+    init_field(3, bool, active, "integer")
+    init_SQL("PRIMARY KEY(`id`)")
+init_close(module)
 
 #include <nxi/data/deinit.hpp>
