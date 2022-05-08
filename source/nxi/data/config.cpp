05d1821cbc1fb65f4f333432a98cc498edcc3aa0
ads00 <ads.neuroshok@gmail.com>
2022-05-09 01:02:37 +0200

    [fix] config get query

--- source/nxi/data/config.cpp
+++ source/nxi/data/config.cpp
@@ -18,7 +18,7 @@ namespace nxi::data::config::internal
 {
     void prepare(nxi::database& db)
     {
-        db.prepare(prepared_query::get_config_value, "SELECT value, session_id FROM config WHERE key = ? AND session_id = ?");
+        db.prepare(prepared_query::get_config_value, "SELECT * FROM config WHERE key = ? AND session_id = ?");
         db.prepare(prepared_query::set_config_value, "INSERT OR REPLACE INTO config(key, value, session_id) VALUES(?, ?, ?)");
     }
 } // nxi::data::config::internal
